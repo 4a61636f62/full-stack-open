a8fd65e7b4d7502a8d555cf2f0b2f8fdef1f5839
Jacob Poole <jacob@pop-os.localdomain>
2021-05-04 15:34:09 +0100

    Added ESLint and fixed errors

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -2,37 +2,37 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 mongoose.connect(process.env.MONGODB_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
-        .then(() => {
-            console.log('connected to MongoDB')
-        })
-        .catch(reason => {
-            console.log('failed to establish database connection:', reason)
-            process.exit(1)
-         })
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(reason => {
+    console.log('failed to establish database connection:', reason)
+    process.exit(1)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 3,
-        unique: true,
-    },
-    number: {
-        type: String,
-        required: true,
-        minlength: 8
-    }
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    unique: true
+  },
+  number: {
+    type: String,
+    required: true,
+    minlength: 8
+  }
 })
 
 personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
