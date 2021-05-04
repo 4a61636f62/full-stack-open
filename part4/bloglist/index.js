62bdc7774a4211f0642cbb93fdeff78b8365b3e8
Jacob Poole <jacob@pop-os.localdomain>
2021-05-04 18:20:09 +0100

    git config

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,25 +1,11 @@
-const config = require('./utils/config')
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogsRouter = require('./controllers/blogs')
+const server = http.createServer(app)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('failed to establish connection to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
+
