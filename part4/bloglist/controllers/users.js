32ced041e0e1afad9694dc29b1205a46f76a9f46
Jacob Poole <4a61636f62@gmail.com>
2021-05-12 10:54:36 +0100

    completed all exercises up to 6.3

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -8,10 +8,9 @@ usersRouter.post('/', async (request, response, next) => {
   if (body.password.length < 3) {
     return next({
       name: 'ValidationError',
-      message: "User validation failed: password is shorter than the minimum allowed length (3)."
+      message: 'User validation failed: password is shorter than the minimum allowed length (3).'
     })
   }
-
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
@@ -28,7 +27,7 @@ usersRouter.post('/', async (request, response, next) => {
 })
 
 usersRouter.get('/', async (request, response, next) => {
-  const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1})
+  const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1 })
   response.json(users)
 })
 
